29f11c037ff390e113494cc3ecf58f08a0ab68fe
Ruslan Pilin <dohque@gmail.com>
2013-11-27 08:53:09 +0400

    draft applying comingsoon template

--- application.js
+++ application.js
@@ -1,24 +1,28 @@
+var express = require('express');
+
 var application = function Application() {
-
-  process.env.PWD = process.cwd()
-
-  var express = require('express');
   var app = express();
-
+  
+  process.env.PWD = process.cwd();
   app.use(express.static(process.env.PWD + '/static'));
-  app.use(express.logger('dev'));
 
   app.set('views', process.env.PWD + '/views')
   app.set('view engine', 'jade')
+
+  app.use(express.logger('dev'));
 
   app.get('/hello.txt', function(req, res){
     res.send('Hello World');
   });
 
   app.get('/', function (req, res) {
-    res.render('index', { title : 'Home' })
+    res.render('comingsoon');
   });
-
+/*
+  app.get('/', function (req, res) {
+    res.render('index', { title: "Home" });
+  });
+*/
   app.listen(3000);
   console.log('Listening on port 3000');
 }
